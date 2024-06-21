46648e46e85a38f3bacfaf58ad4ba05939add4b4
NAHDI DUTA AHMAD <dutaahmadtefur@gmail.com>
2024-06-22 04:20:14 +0700

    init porto with simple HTML

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: "class", // or 'media' or 'class'
+	purge: ["./*.html"],
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
